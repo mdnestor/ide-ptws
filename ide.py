3a5cfde297482cbf5443e054fd3a54c5e30356af
mdnestor <mdnest01@louisville.edu>
2022-02-03 15:21:50 -0500

    Updated methods

--- ide.py
+++ ide.py
@@ -8,14 +8,17 @@ class IDE():
         self.u  = initial_condition
 
 class IDESimulation():
-    def __init__(self, ide, num_iters, xmin, xmax, step_size, boundary_condition = 'nearest'):
-        self.ide = ide
-        n = 1 + int(1 + (xmax - xmin) / step_size)
-        self.domain = np.linspace(xmin, xmax, n)
-        self.boundary_condition = boundary_condition
-        self.results = self.run(num_iters)
+    def __init__(self, ide, num_iters, domain_size, step_size, boundary_condition = 'nearest'):
+        n = 1 + int(1 + domain_size/step_size)
 
-    def run(self, num_iters):
+        self.ide = ide
+        self.domain = np.linspace(-domain_size/2, domain_size/2, n)
+        self.boundary_condition = boundary_condition
+
+        self.run(num_iters)
+        self.plot()
+
+    def run(self, num_iters, plot=True):
         x = self.domain
         U = np.zeros((num_iters + 1, x.shape[0]))
 
@@ -26,4 +29,56 @@ class IDESimulation():
             kx = k(x)
             U[i + 1] = ndimage.convolve(g(U[i]), kx, mode =
             self.boundary_condition) / np.sum(kx)
-        return U
+        self.results = U[:,::-1]
+
+    def plot(self):
+        self.plot_heatmap()
+        self.plot_diffmap()
+
+    def plot_heatmap(self):
+        x = self.domain
+        y = np.arange(self.results.shape[0])
+
+        X, Y = np.meshgrid(x, y)
+        Z = self.results
+
+        fig, ax = plt.subplots()
+
+        c = ax.pcolormesh(X, Y, Z,
+                          cmap='coolwarm',
+                          vmin=np.min(Z),
+                          vmax=np.max(Z))
+
+        ax.axis([np.min(X), np.max(X), np.min(Y), np.max(Y)])
+
+        fig.colorbar(c, ax=ax)
+
+        ax.set_xlabel('$x$ (position)')
+        ax.set_ylabel('$n$ (time step)')
+
+        plt.show()
+
+    def plot_diffmap(self):
+        x = self.domain
+        y = np.arange(self.results.shape[0])
+
+        X, Y = np.meshgrid(x, y)
+        Z = self.results
+        Z = np.gradient(Z, axis=0)
+        Z /= np.sign(Z)
+
+        fig, ax = plt.subplots()
+
+        c = ax.pcolormesh(X, Y, Z,
+                          cmap='coolwarm',
+                          vmin=np.min(Z),
+                          vmax=np.max(Z))
+
+        ax.axis([np.min(X), np.max(X), np.min(Y), np.max(Y)])
+
+        fig.colorbar(c, ax=ax)
+
+        ax.set_xlabel('$x$ (position)')
+        ax.set_ylabel('$n$ (time step)')
+        plt.gca().invert_yaxis()
+        plt.show()
