b699cbe9e37cd31be6a88bcd0707c793d38e4216
mdnestor <mdnest01@gmail.com>
2023-04-27 02:11:31 -0400

    Create 01_monotone_growth.py

---
+++ examples/01_monotone_growth.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+from ide import simulate_integrodifference
+
+x = np.linspace(-8, 8, 2**10)
+dx = (x[-1] - x[0]) / len(x)
+
+# Gaussian kernel
+r = 4
+K = stats.norm.pdf(np.arange(-r, r+dx, dx))
+K /= np.sum(K)
+
+u0 = 0.5 * np.heaviside(-2.0 - x, 1.0)
+f = lambda y: 2.0 * y * (1.0 - y)
+
+soln = simulate_integrodifference(x, u0, K, f, 10)
+
+for i in range(soln.shape[0]):
+  plt.plot(x, soln[i], color="blue")
+plt.show()
+plt.savefig("example_01a.png")
+
+plt.imshow(soln, aspect="auto", extent=[x[0], x[-1], 0, soln.shape[0]])
+plt.colorbar()
+plt.show()
+plt.savefig("example_01b.png")
