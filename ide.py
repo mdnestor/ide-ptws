620d1356bd4d54a221d3b730deff73c96ff62445
mdnestor <mdnest01@gmail.com>
2023-06-21 15:17:55 -0400

    better array handling

--- ide.py
+++ ide.py
@@ -3,14 +3,15 @@ from scipy import ndimage
 from typing import Callable
 
 def simulate_integrodifference(x: np.ndarray, u0: np.ndarray, K: np.ndarray, f: Callable, n_steps: int):
-  soln = [u0]
-  for i in range(n_steps):
-    u = soln[-1]
-    L, R = u[0], u[-1]
+  soln = np.zeros((n_steps + 1,) + u0.shape)
+  soln[0] = u0
+  for t in range(n_steps):
+    u = soln[t]
+    L = u[0]
+    R = u[-1]
     fu = np.apply_along_axis(f, 0, u)
     u_next = ndimage.convolve(fu, K, mode="nearest")
     u_next[0] = f(L)
     u_next[-1] = f(R)
-    soln.append(u_next)
-  soln = np.array(soln)
+    soln[t + 1] = u_next
   return soln
